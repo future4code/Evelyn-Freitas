0bf25064339cb2c3e52a81cee07615b84010af53
EvelynD95 <evelyn.dantas95@gmail.com>
2020-10-16 16:59:04 -0300

    Práticas com Listas e Estilização

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -3,33 +3,44 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    pessoas: [
+      {
+        nomeUsuario: 'Paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?random=1'
+      },
+      {
+        nomeUsuario: 'Paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?random=2'
+      },
+      {
+        nomeUsuario: 'Paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?random=3'
+      },
+      {
+        nomeUsuario: 'Paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?random=4'
+      },
+      {
+        nomeUsuario: 'Paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?random=5'
+      }
+    ]
+  };
+  
   render() {
     return (
       <div className={'app-container'}>
         <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150?random=1'}
+        {},
+        {},
+        {}
         />
-
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150?random=2'}
-        />
-
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150?random=3'}
-        />
-
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150?random=4'}
-        />
-
       </div>
     );
   }

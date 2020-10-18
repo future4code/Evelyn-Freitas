c8c4d4618d4684fb61ee79b297e8d161fec6d717
EvelynD95 <evelyn.dantas95@gmail.com>
2020-10-17 21:01:58 -0300

    Algumas alterações no App.js

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -30,16 +30,56 @@ class App extends React.Component {
         fotoUsuario: 'https://picsum.photos/50/50',
         fotoPost: 'https://picsum.photos/200/150?random=5'
       }
-    ]
+    ],
+
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+
+  };
+
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    };
+
+    const inserirNovoPost = [...this.state.pessoas, novoPost];
+
+    this.setState = ({ pessoas: inserirNovoPost });
+  };
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({ valorInputNomeUsuario: event.target.value });
+  };
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+  };
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({ valorInputFotoPost: event.target.value });
   };
   
   render() {
+    
+    const formPost = this.state.pessoas.map((pessoa) => {
+      return (
+        <p>
+          {pessoa.nomeUsuario},
+          {pessoa.fotoUsuario},
+          {pessoa.fotoPost}
+        </p>
+      );
+    });
+
     return (
       <div className={'app-container'}>
-        <Post
-        {},
-        {},
-        {}
+        <input
+          nomeUsuario={this.state.valorInputFotoUsuario}
+          fotoUsuario={this.state.valorInputFotoUsuario}
+          fotoPost={this.state.valorInputFotoPost}
         />
       </div>
     );

5743711e06d2c213f7dd2e8d99ed7eb40a52a64f
EvelynD95 <evelyn.dantas95@gmail.com>
2020-10-29 20:03:29 -0300

    Tentativa de renderização condicional

--- semana7/labeboredAPI/labebored-api/src/components/HomePage.js
+++ semana7/labeboredAPI/labebored-api/src/components/HomePage.js
@@ -2,25 +2,31 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import BoredApi from './BoredApi';
-import LyricsApi from './LyricsApi';
+import LyricsApi from './LyricsApi';        
 
 class HomePage extends React.Component {
     state = {
-        homePage: true
-    }
-            
-    onClickChangePage = (event) => {
-        this.setState({ homePage: !this.state.homePage });
+        homePage:false
     }
 
+    onClickChangePage = () => {
+        this.setState({ homePage: !this.state.homePage });
+    }
+    
     render() {
-        const currentPage = this.state.homePage ? <BoredApi /> : <LyricsApi />;
+        const renderedPage = () => {
+            if (this.state.homePage) {
+                <BoredApi />
+            } else {
+                <LyricsApi />
+            }
+        }
         return (
             <div>
                 <h1>Meet differents APIs</h1>
                 <p>Click on one of the buttons bellow:</p>
-                <button onClick={this.onClickChangePage}>Meet Bored API</button>
-                <button onClick={this.onClickChangePage}>Meet Lyrics API</button>
+                <button onClick={() => this.onClickChangePage(renderedPage)}>Meet Bored API</button>
+                <button onClick={() => this.onClickChangePage(renderedPage)}>Meet Lyrics API</button>
             </div>
         );
     }

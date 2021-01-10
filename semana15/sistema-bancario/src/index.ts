28908efa3c00ad5eff438d8a64cbc2406293c789
Evelyn <evelyn.dantas95@gmail.com>
2021-01-09 21:02:02 -0300

    Requisitos mínimos feitos com ajuda da resolução

--- semana15/sistema-bancario/src/index.ts
+++ semana15/sistema-bancario/src/index.ts
@@ -1,65 +1,59 @@
 import express, { Request, Response, Express } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
+import { accounts } from "./accounts"
+import e from "express";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-type conta = {
-    nome: string,
-    CPF: number,
-    dataDeNascimento: string,
-    saldo: number,
-    extrato: any[]  
-}
+app.post("/users/create", (req: Request, res: Response) => {
+    try {
+        const { name, CPF, dateOfBirth } = req.body;
+        
+        const [day, month, year] = dateOfBirth.split("/");
 
-type transacoes = {
-    
-}
+        const birthDay: Date = new Date(`${year}-${month}-${day}`);
 
-let users: conta[] = [
-    {
-        nome: "Victória Mattos",
-        CPF: 12323245611,
-        dataDeNascimento: "10/12/2000",
-        saldo: 0,
-        extrato: [
-            {
-                valor: 0, 
-                data: "02/01/2021", 
-                descricao: "Saldo zerado, nenhuma movimentação na conta"
-            },
-        ]    
-    },
-    {
-        nome: "Pedro Henrique",
-        CPF: 32154513155,
-        dataDeNascimento: "22/08/1985",
-        saldo: 0,
-        extrato: [
-            {
-                valor: 0, 
-                data: "01/01/2021", 
-                descricao: "Saldo zerado, nenhuma movimentação na conta"
-            },
-        ]    
-    },
-    {
-        nome: "Carol Nascimento",
-        CPF: 17585435411,
-        dataDeNascimento: "02/05/1995",
-        saldo: 0,
-        extrato: [
-            {
-                valor: 0, 
-                data: "15/12/2020", 
-                descricao: "Saldo zerado, nenhuma movimentação na conta"
-            },
-        ]    
-    }    
-]
+        const ageInMilliseconds: number = Date.now() - dateOfBirth.getTime();
+
+        const ageInYears: number = ageInMilliseconds / 1000 / 60 / 60 / 24 / 365;
+
+        if (ageInYears < 18) {
+            res.statusCode = 406
+            throw new Error("A idade deve ser maior que 18 anos.");
+        }
+
+        accounts.push({
+            name,
+            CPF,
+            birthDay,
+            balance: 0,
+            statement: []
+        })
+
+        res.status(201).send("Conta criada com sucesso!")
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error.message)
+    }
+});
+
+app.get("/users/all", (req: Request, res: Response) => {
+    try {
+        if (!accounts.length) {
+            res.statusCode = 404
+            throw new Error("Conta não encontrada.")
+        } 
+
+        res.status(200).send(accounts)
+    } catch (error) {
+        console.log(error);
+        res.send(error.message)        
+    }   
+});
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
